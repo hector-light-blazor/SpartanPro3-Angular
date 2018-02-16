c03ec17e65c7dcbea9c11dc1b8adef73cf187ddf
HCHAPA <hchapa@911.local>
2018-02-16 14:54:10 -0600

    quick search added the comments section..

--- src/app/quick-search/quick-search.component.ts
+++ src/app/quick-search/quick-search.component.ts
@@ -12,6 +12,8 @@ export class QuickSearchComponent implements OnInit {
   dataTable:any = [];
   searchTable:string="";
   isLoading: boolean = true;
+  comments: any = [];
+  selectedRow: any = null;
   constructor(private route: ActivatedRoute,private router: Router, private app: AppService) { }
 
   ngOnInit() {
@@ -22,7 +24,8 @@ export class QuickSearchComponent implements OnInit {
     this.app.GET_METHOD(this.app.route.api.gMultiSearch + this.route.snapshot.params['search']).subscribe((response:any) => {
 
       response.forEach(row => {
-           
+         row.cdisplay = false;
+         row.comments = [];
            if(row.sentto == "00") {
               row.sentto = "ARCHIVE";
            }else {
@@ -58,6 +61,8 @@ export class QuickSearchComponent implements OnInit {
     this.app.GET_METHOD(this.app.route.api.gMultiSearch + this.route.snapshot.params['search']).subscribe((response:any) => {
 
       response.forEach(row => {
+           row.cdisplay = false;
+           row.comments = [];
            if(row.sentto == "00") {
               row.sentto = "ARCHIVE";
            }else {
@@ -73,4 +78,65 @@ export class QuickSearchComponent implements OnInit {
     });
   }
 
+  openComments(row) {
+      
+      if(row.cdisplay) {
+       
+        row.cdisplay = false;
+        return;
+      }
+
+      if(this.selectedRow) {
+        this.selectedRow.cdisplay = false;
+      }
+      this.selectedRow = row;
+      
+      row.cdisplay = true;
+
+      //Lets Fetch the comments...
+      row.comments = [];
+      this.app.GET_METHOD(this.app.route.api.vtFeeds + row.id_ticket).subscribe((response: Array<FEED>) => {
+        
+          for(let com of response){
+             
+             // Do hack on time to work on all browsers Edge, Firefox and Chrome..
+             // It works awesome on Chrome but I don't want to support only chrome..
+             let timeSplit = com.time_track.split(" ");
+             timeSplit[0] = timeSplit[0].split("-");
+             
+             timeSplit[0] = timeSplit[0][1] + '/' + timeSplit[0][2] + '/' + timeSplit[0][0];
+             timeSplit[1] = timeSplit[1].substr(0, timeSplit[1].indexOf('.'));
+             timeSplit[1] = timeSplit[1].split(':'); // so we can get array and fix the time issue...
+             timeSplit[1][0] = (parseInt(timeSplit[1][0]) <  7) ? parseInt(timeSplit[1][0]) + 6 : parseInt(timeSplit[1][0]) - 6; // Make central time from universal time...
+             timeSplit[1] = timeSplit[1][0] + ':' + timeSplit[1][1] + ':' + timeSplit[1][2];
+             com.time_track = timeSplit[0] + ' ' + timeSplit[1]; // Final Results...
+            
+             let now = new Date();
+             let track:Date = new Date(com.time_track);
+
+             let format = this.app.FORMAT_AMPM(new Date(com.time_track));
+             console.log(format);
+             com.edit = false;
+             com.allow = (com.user_id == this.app.account_info.user_id);
+             com.time = (track.getMonth() + 1)  + "/" + track.getDate() + "/" + track.getFullYear() + " " + format;//this.app.FORMAT_AMPM(new Date(com.time_track));
+          }  
+          row.comments = response;
+      });
+  }
+
+}
+
+// Create Interface For Comments
+interface FEED {
+  id_com:          string,
+  user_id:         string,
+  first_name:      string,
+  last_name:       string,
+  time:            string,
+  ticket_comments: string,
+  ticket_number:   string,
+  ticket_section:  string,
+  allow?: boolean;
+  edit?: boolean;
+  time_track:      any
 }
