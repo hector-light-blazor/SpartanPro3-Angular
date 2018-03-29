977cf2e3708d851740ad7fb5e987e40418c241bc
Programmer <programmer@Programmers-MacBook-Pro.local>
2018-03-28 22:17:37 -0500

    fix the problem with the module for breaking.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -53,7 +53,7 @@ const routes: Routes = [
   },
   {
     path: 'subdivision',
-    loadChildren: '.subdivision/subdivision.module#SubdivisionModule'
+    loadChildren: './subdivision/subdivision.module#SubdivisionModule'
   }
 
 ];
