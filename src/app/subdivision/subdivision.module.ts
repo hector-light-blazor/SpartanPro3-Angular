e3634f25bf0fa3468391e38f7156275c19124f93
HECTOR CHAPA <hchapa@lrgvdc911.org>
2018-04-05 12:25:59 -0500

    changes

--- src/app/subdivision/subdivision.module.ts
+++ src/app/subdivision/subdivision.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedModule } from '../shared/shared.module';
 import { SubdivisionRoutingModule } from './subdivision-routing.module';
 import { WorksheetComponent } from './worksheet/worksheet.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     SubdivisionRoutingModule
   ],
   declarations: [WorksheetComponent]
