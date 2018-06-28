ce71b58a1b058d12f8e5ee8450a077881278ebb2
HECTOR CHAPA <hchapa@lrgvdc911.org>
2018-06-28 16:58:51 -0500

    more udates

--- src/app/settings/settings-routing.module.ts
+++ src/app/settings/settings-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ToolbarIComponent } from './toolbar-i/toolbar-i.component';
 import { ToolbarUComponent } from './toolbar-u/toolbar-u.component';
+import { TicketIComponent } from './ticket-i/ticket-i.component';
+import { TicketUComponent } from './ticket-u/ticket-u.component';
 
 const routes: Routes = [{
   path: "createToolbar",
@@ -10,6 +12,13 @@ const routes: Routes = [{
 {
   path: "updateToolbars",
   component: ToolbarUComponent
+},
+{
+  path: "Ticket",
+  component: TicketIComponent
+},{
+  path: "updateTicket",
+  component: TicketUComponent
 }
 
 ];
